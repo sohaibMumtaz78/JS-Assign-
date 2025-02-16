5c14319a91ef36b1c8407653582959349c630b2e
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-02-17 01:34:30 +0500

    Chapter 5

---
+++ Chapter 5/app.js
@@ -0,0 +1,77 @@
+// console.log("Hello World")
+
+//Chapter 05
+
+//Q1
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Enter second num");
+//     sum = num1 + num2 ;
+
+//     document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+//Q2
+        //subtraction
+
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Enter Second num");
+//     sub = num1 - num2;
+
+//     document.write("Subtraction of " + num1 + " and " + num2 + " is " + sub);
+
+        //Multiplication
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Enter Second num");
+//     Mul = num1 * num2;
+
+//     document.write("Multiplication of " + num1 + " and " + num2 + " is " + Mul);
+
+        //Division
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Enter second num");
+//     division = num1 / num2;
+
+//     document.write("Division of " + num1 + " and " + num2 + " is " + division);
+
+        //Modulus
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Ënter second num");
+//     mod = num1 % num2;
+
+//     document.write("Modulus of " + num1 + " and " + num2 + " is " + mod); 
+
+//Q3
+// var variable;
+//     document.write("value after variable declaration is: " + variable);
+
+//     variable = +prompt("Enter Initial Value");
+//     document.write("</br> Initail value : " + variable);
+
+//     increment = +prompt("Enter icreament in inital value");
+//     incValue = variable + increment;
+//     document.write("</br> Value after increment is : " + incValue);
+
+//     addition = +prompt("Enter num for addition after increment");
+//     addValue = addition + incValue;
+//     document.write("</br> Value after addition is : " + addValue);
+
+//     decrement = +prompt("Enter decrement Value");
+//     decValue = addValue - decrement;
+//     document.write("</br> Value after decrement is : " + decValue);
+
+//     division = +prompt("Enter value of division");
+//     divValue = decValue % division;
+    
+//     document.write("</br> The reminder is : " + divValue);
+
+//Q4
+// var ticketPrice = +prompt("Enter ticket that you buy");
+//     totalPrice = ticketPrice *  600;
+//     document.write("Total cost to  buy " + ticketPrice + " tickets to a movie is " + totalPrice);
+
+
+//Q5
+// var table = +prompt("Enter table num");
+//     times = +prompt("Enter table times");
+//     for(i = 1 ; i <= times ; i++){
+//         document.write(table + " x " + i + " = " + (table * i) + "</br>");
+//     }
