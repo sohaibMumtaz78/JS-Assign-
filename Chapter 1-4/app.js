82ba4f24e8fb789a7336d7fdffc59489b773d9df
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-02-16 17:01:34 +0500

    chapter 1-3 assignment

---
+++ Chapter 1-4/app.js
@@ -0,0 +1,96 @@
+// console.log("hello world")
+
+//Chapter 01
+
+// Q1
+// alert("Hello World")
+
+
+// Q2
+// alert("Error! Please Enter a password")
+
+// Q3
+// alert("Welcome to JS Land...\nHappy coding!0")
+
+//Q4
+// alert("Welcome to JS Land...")
+
+// alert("Happy coding! \n Prevent this page from creating additional dialogs")
+
+
+//Q5
+// alert("Hello...I can run JS through my web browser's console")
+
+//Chapter 02
+
+//Q1
+// var userName = "Sohaib Mumtaz";
+// console.log("UserName" , userName);
+
+//Q2
+// var myName = "Sohaib Mumtaz";
+// console.log("fullName = ", myName);
+
+//Q3
+// var message = "Hello World";
+// alert(message);
+
+//Q4
+// var studentName = "Sohaib Mumtaz";
+//     age = "20";
+//     course = "Web & App Development";
+// alert(studentName);
+// alert(age);
+// alert(course);`
+
+//Q5
+// var word = "Pizza";
+// output ="";
+// for( i = word.length ; i > 0 ; i--){
+//     output += word.slice(0,i) + "\n";
+// }
+//  alert(output);
+
+// Q6
+// var emailAddress = "sohaibmumtaz78@gmail.com";
+// alert("My Email Address is " + emailAddress)
+
+// Q7
+// var book = "A smarter way to learn JavaScript";
+// alert(book)
+
+//Q8
+// document.write("Yah! I can write HTML content through JavaScript")
+
+//Q9
+// var design ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+
+
+//Chapter 03
+//Q1
+// var age = "19";
+// alert("I am " + age + " years old")
+
+
+//Q2
+// var visitWebpage =  +prompt("Enter how many times you visited this website");
+// alert("You have visited this site " + visitWebpage + " times")
+
+//Q3
+// var birthYear = +prompt("Enter your birth year ") ;
+// document.write("My birt year is " + birthYear + "<br> Data type of my decleared variable is number" );    
+
+// Q4
+//  var buyName = "Sohaib Mumtaz";
+//     productTitle = "5"; 
+//     storeName = "Durrat Shop";
+// document.write(buyName + " ordered " + productTitle + " T-shirts on " + storeName + " clothing store");
+
+//Chapter 04
+var userName = "Sohaib Mumtaz";
+    age = "20";
+    email = "sohaibmumtaz78@gmail.com";
+    console.log( " Name " + userName + "\n age " + age + "\n email " + email )
+
+
