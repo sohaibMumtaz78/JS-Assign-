9f076dac0b4ab3f13caa68e6f32a44f808e22365
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-02-17 15:38:06 +0500

    chapter 05 complete

---
+++ Chapter 05/app.js
@@ -0,0 +1,160 @@
+// console.log("Hello World")
+
+//Chapter 05
+
+//Q1
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Enter second num");
+//     sum = num1 + num2 ;
+
+//     document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+//Q2
+        //subtraction
+
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Enter Second num");
+//     sub = num1 - num2;
+
+//     document.write("Subtraction of " + num1 + " and " + num2 + " is " + sub);
+
+        //Multiplication
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Enter Second num");
+//     Mul = num1 * num2;
+
+//     document.write("Multiplication of " + num1 + " and " + num2 + " is " + Mul);
+
+        //Division
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Enter second num");
+//     division = num1 / num2;
+
+//     document.write("Division of " + num1 + " and " + num2 + " is " + division);
+
+        //Modulus
+// var num1 = +prompt("Enter first num");
+//     num2 = +prompt("Ënter second num");
+//     mod = num1 % num2;
+
+//     document.write("Modulus of " + num1 + " and " + num2 + " is " + mod); 
+
+//Q3
+// var variable;
+//     document.write("value after variable declaration is: " + variable);
+
+//     variable = +prompt("Enter Initial Value");
+//     document.write("</br> Initail value : " + variable);
+
+//     increment = +prompt("Enter icreament in inital value");
+//     incValue = variable + increment;
+//     document.write("</br> Value after increment is : " + incValue);
+
+//     addition = +prompt("Enter num for addition after increment");
+//     addValue = addition + incValue;
+//     document.write("</br> Value after addition is : " + addValue);
+
+//     decrement = +prompt("Enter decrement Value");
+//     decValue = addValue - decrement;
+//     document.write("</br> Value after decrement is : " + decValue);
+
+//     division = +prompt("Enter value of division");
+//     divValue = decValue % division;
+    
+//     document.write("</br> The reminder is : " + divValue);
+
+//Q4
+// var ticketPrice = +prompt("Enter ticket that you buy");
+//     totalPrice = ticketPrice *  600;
+//     document.write("Total cost to  buy " + ticketPrice + " tickets to a movie is " + totalPrice);
+
+
+//Q5
+// var table = +prompt("Enter table num");
+//     times = +prompt("Enter table times");
+//     for(i = 1 ; i <= times ; i++){
+//         document.write(table + " x " + i + " = " + (table * i) + "</br>");
+//     }
+
+//Q6
+// var Celsius = +prompt("Enter Celsius num");
+//         Fahrenheit = (Celsius * 9/5)+32 ;
+//         document.write(Celsius + " °C is " + Fahrenheit + " °F </br> ")
+
+// var Fahrenheit = +prompt("Enter Fahrenheit num");
+//         Celsius = (Fahrenheit - 32) * 5/9 ;
+//         document.write(Fahrenheit + "°F is " + Celsius + " °C" )
+
+
+//Q7
+// var price1 = +prompt("Enter price of 1st item ");
+//         document.write("Price of 1st item is " + price1  + "</br>");
+//         price2 = +prompt("Enter price of 2nd item ");
+//         document.write("Price of 2nd item is " + price2 + "</br>");
+//         orderQuanlity1 = +prompt("Enter Quantity of 1st item ");
+//         document.write("Quantity of 1st item is " + orderQuanlity1 + "</br>");
+//         orderQuanlity2 = +prompt("Enter quantity of 2nd itme ");
+//         document.write("Quantity of 2nd item is "+ orderQuanlity2 + "</br>");
+//         deliveryChargers = 100;
+//         document.write("Shipping Charges is " + deliveryChargers + "</br>");
+
+//         priceOf1 = price1 * orderQuanlity1 ;
+//         priceOf2 = price2 * orderQuanlity2 ;
+//         totalcost = priceOf1 + priceOf2 + deliveryChargers ;
+//         document.write("Total cost of your order is " + totalcost);
+
+//Q8
+// var marksObtained = +prompt("Enter Obtain Marks ");
+//         totalmarks = +prompt("Enter Total Marks ");
+//         percentage = (marksObtained * 100) / totalmarks;
+//         document.write("Obtained marks is " + marksObtained + "</br>" );
+//         document.write("total marks is " + totalmarks + "</br>");
+//         document.write("Percentage is " + percentage);
+
+
+//Q9
+// var usDollar = +prompt("Enter us dollar");
+//         saudiRiyal = +prompt("Enter saudi Riyal");
+//         dollarTopkr = usDollar * 104.80 ;
+//         riyalTopkr = saudiRiyal * 28 ;
+//         currencyTopkr = dollarTopkr + riyalTopkr ;
+//         document.write("Total US Dollar : " + usDollar + "</br>");
+//         document.write("Total Saudi Riyal : " + saudiRiyal + "</br>");
+//         document.write("Total Currency in PKR : " + currencyTopkr);
+
+
+//Q10
+// var num = +prompt("Enter Num");
+//         result = ((num + 5)*10)/2;
+//         document.write("Result is : " + result);
+
+
+//Q11
+// var birthYear = +prompt("Enter birth year");
+//         currentYear = +prompt("Enter Current Year");
+//         age = currentYear - birthYear ;
+//         document.write("Your Age is : " + age);
+
+//Q12
+// var radius = +prompt("Enter Radius of circle");
+//         pi = 3.142;
+//         circumferenceValue = 2 * pi * radius ;
+//         area = pi * radius * radius;
+//         document.write("Radius of circle : " + radius + "</br>");
+//         document.write("The circumference : " + circumferenceValue  + "</br>");
+//         document.write("The area is : " + area);
+
+//Q13
+// var favSnack = +prompt("Enter favourite Snack");
+//         currentAge = +prompt("Enter Current Age");
+//         MaximumAge = +prompt("Enter Maximum Age");
+//         snacksPerday = +prompt("Enter amount of Sanck per day ");
+//         result = ((MaximumAge - currentAge) * snacksPerday);
+
+//         document.write("Your Fav Sncak : " + favSnack + "</br>");
+//         document.write("Current Age : " + currentAge + "</br>");
+//         document.write("Maximum Age : " + MaximumAge + "</br>");
+//         document.write("You will need " +result + " to last you until the ripe old age of " + MaximumAge);        
+
+
+
