fe12fc8928d8b3ca44b6e066204ede6e9301cf48
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-02-24 18:29:43 +0500

    chapter 12-15

---
+++ Chapter 12-13/app.js
@@ -0,0 +1,91 @@
+// console.log("hello")
+
+//Q1
+// var num = prompt("Enter Value");
+//     characterValue(num){
+//         if 
+//     }
+
+// var num = prompt("Enter value");
+//     function checkCharacterType(num) {
+//     if (num.length !== 1) {
+//         return "Please enter a single character.";
+//     }
+    
+//     let ascii = num.charCodeAt(0);
+    
+//     if (ascii >= 48 && ascii <= 57) {
+//         return "The character is a number.";
+//     } else if (ascii >= 65 && ascii <= 90) {
+//         return "The character is an uppercase letter.";
+//     } else if (ascii >= 97 && ascii <= 122) {
+//         return "The character is a lowercase letter.";
+//     } else {
+//         return "The character is neither a number nor a letter.";
+//     }
+// }
+
+// // Example Usage
+// console.log(checkCharacterType('A')); // The character is an uppercase letter.
+// console.log(checkCharacterType('z')); // The character is a lowercase letter.
+// console.log(checkCharacterType('5')); // The character is a number.
+// console.log(checkCharacterType('@')); // The character is neither a number nor a letter.
+
+//Q2
+// var num1 = prompt("Enter first integer Num");
+//     num2 = prompt("Enter second integer num");  
+//     if (num1 > num2){
+//         console.log("Num1 is Larger " + num1);
+//         // return
+//     }else if (num1 < num2){
+//         console.log("Num2 is lager " + num2);
+//     }else if(num1 === num2){
+//         console.log("Both Numbers are equal " + num1);
+        
+//     }
+
+
+//Q3
+// var num = +prompt("Enter Number");
+//     if(num < 0){
+//         console.log("Number is negative " + num);
+//     }else{
+//         console.log("Number is Positive " + num)
+//     }
+
+//Q4
+// var value = prompt("Enter alphabet");
+// if (value === "a"){
+//     console.log("Aphabet is vowel : " + value);
+// }else if(value === "e"){
+//     console.log("Aphabet is vowel : " + value);
+// }else if(value === "i"){
+//     console.log("Aphabet is vowel : " + value);
+// }else if(value === "o"){
+//     console.log("Aphabet is vowel : " + value);
+// }else if(value === "u"){
+//     console.log("Aphabet is vowel : " + value);
+// }else if(value === "s"){
+//     console.log("Aphabet is vowel : " + value);
+// }else{
+//     console.log("Aphabet is not vowel : " + value);
+// }
+
+//Q5
+// var password1 = prompt("Enter password");
+//     password2 = prompt("Enter confirm password");
+//     if(password1 === password2){
+//         console.log("Password Matched : " + password1);
+//         // return;
+//     }else if(password1 != password2){
+//         console.log("wrong password please correct password ");
+//     }
+
+//Q6
+// var hour = prompt("Enter hours");
+// var greeting = (hour < 18) ? "Good day" : "Good evening";
+// console.log(greeting); // Output: "Good day"
+
+
+
+ 
---
+++ Chapter 13-15/app.js
@@ -0,0 +1,80 @@
+// console.log("Chapter 13-15")
+
+// let studentNames = [];
+// studentNames.push("Ali");
+// studentNames.push("Ayesha");
+// console.log(studentNames); // Output: ["Ali", "Ayesha"]
+
+
+
+//literal notation mtlb empty arrray myn value push krwana 
+//Q1 
+// var studentName = [];
+// studentName.push("Sohaib");
+// studentName.push("Mumtaz");
+// console.log(studentName)
+
+
+
+//object notation  mtlb array myn pehle he value assign kr dyna phr call krwana 
+//Q2
+// var student = {
+//     name: "sohaib Mumtaz",
+//     age: "20",
+//     course: "web & APP Dev"
+// }
+// console.log(student)
+
+//Strings in array
+//Q3
+// var personName = [ "Sohaib", "Fahad", "Asad", "Arslan"];
+// console.log(personName);
+
+//Numbers in array
+//Q4
+// var num = ["009", "035", "045", "032"];
+// console.log(num)
+
+//Bolean array mtlb jo array true or false py depend karein
+//Q5
+// var Bolean = ["true" , "false" , "true" ,"false"];
+// console.log(Bolean);
+
+
+//Q6
+// var mixedArray = ["Sohaib","20","true" ]
+// console.log("Name : " + mixedArray[0]);
+// console.log("Age : " + mixedArray[1]);
+// console.log("Information is : " + mixedArray[2]);
+
+
+
+//Q7
+// var qualif = ["SSC" , "HSC" , "BSC" , "BS", "BCOM", 'MS' , 'M.Phil ',"PhD"]
+// // console.lo
+// document.write("<h4>Qualification</h4>")
+// document.write(qualif[0]+"</br>");
+// document.write(qualif[1]+"</br>");
+// document.write(qualif[2]+"</br>");
+// document.write(qualif[3]+"</br>");
+// document.write(qualif[4]+"</br>");
+// document.write(qualif[5]+"</br>");
+// document.write(qualif[6]+"</br>");
+// document.write(qualif[7]+"</br>");
+
+//Q8
+    // var studentName = ['Sohaib' , 'Awais' , 'Umar' , 'Asad'];
+    // var studentScore = ['380' , '310' , '210', '120'];
+    // var totalMarks = 500;
+    //     // document.write("</br> Score of "+ studentName[0] + ' is ' + studentScore[0]+". Percentage : " + totalMarks)
+    //     for(var i = 0 ;i < studentName.length ; i++ ){   
+    //         var Percentage = (studentScore[i]/totalMarks)*100;
+    //         // document.write('</br>Score of ""${studentName[i]} is ${studentScore[i]}.Percentage is ${totalMarks} </br>')
+    //         document.write(`</br>${studentName[i]} scored ${studentScore[i]} out of ${totalMarks}. Percentage: ${Percentage.toFixed(2)}% `);
+    //     }
+
+
+
+
+
+
